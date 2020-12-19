078f5cbc2a92701d6a340a955b1867a4b7b4684f
asliceofpizza007 <a89884526@hotmail.com.tw>
2020-12-19 14:28:27 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo"
+     src="./assets/logo.png"
+  >
 </template>
 
 <script lang="ts">
