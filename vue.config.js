6be4709b19dbf7e420426592416949bc8ed0e83c
裕介 <20190808@cyadmk.com>
2021-01-07 11:15:18 +0800

    update webpack.config

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  publicPath: '/',
+  devServer: {
+    port: 9000
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].[hash].js',
+      chunkFilename: '[name].[chunkhash].js'
+    },
+    resolve: {
+      alias: {
+        '@as': '@/assets',
+        '@c': '@/components',
+        '@v': '@/views',
+        '@img': '@/assets/img',
+        '@scss': '@/assets/scss'
+      }
+    }
+  },
+  productionSourceMap: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "@scss/mixins.scss";'
+      }
+    }
+  }
+
+}
