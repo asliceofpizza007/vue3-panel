f6b531817c3cad26d26d9ebb7a45e77b94c7bb96
asliceofpizza007 <a89884526@hotmail.com.tw>
2021-01-16 11:31:30 +0800

    set minimize area at initial point

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import MinimizeContainer from '@/components/PanelMinimizeContainer.vue'
+import Panel from '@/components/Panel.vue'
 import '@/assets/scss/icon/iconfont.css'
 
-createApp(App).use(store).mount('#app')
+const app = createApp(App)
+const panel = {
+  install () {
+    app.component('Panel', Panel)
+    const el = document.createElement('div')
+    document.body.appendChild(el)
+    createApp(MinimizeContainer).mount(el).$nextTick(() => {
+      document.body.removeChild(el)
+    })
+  }
+}
+
+app.use(store).use(panel).mount('#app')
